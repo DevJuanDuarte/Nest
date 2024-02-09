3c55cc052501eee8ede5ae85d4ff5fe0e1801645
Juan Duarte <juan.devcontact@gmail.com>
2024-02-09 11:39:32 -0500

    Crear Servicio SEED para cargar datos

--- car-dealer-ship/src/app.module.ts
+++ car-dealer-ship/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
 import { BrandsModule } from './brands/brands.module';
 import { CarmodelModule } from './carmodel/carmodel.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [CarsModule, BrandsModule, CarmodelModule],
+  imports: [CarsModule, BrandsModule, CarmodelModule, SeedModule],
   controllers: [],
   providers: [],
   exports: [],
